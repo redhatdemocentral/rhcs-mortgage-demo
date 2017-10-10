e136f64dd62f4c1d80adac6d7d35ace05a7d0c43
Eric D. Schabell <eric@schabell.org>
2017-10-10 18:04:39 -0500

    Updated to use generic routes.

--- init.bat
+++ init.bat
@@ -176,7 +176,7 @@ if not "%ERRORLEVEL%" == "0" (
 echo.
 echo Creating an externally facing route by exposing a service...
 echo.
-call oc expose service %OCP_APP% --hostname=%OCP_APP%.%HOST_IP%.xip.io
+call oc expose service %OCP_APP% --port=8080
 
 if not "%ERRORLEVEL%" == "0" (
   echo.
@@ -190,7 +190,7 @@ echo ====================================================================
 echo =                                                                  =
 echo =  Login to start exploring the Mortgage project:                  =
 echo =                                                                  =
-echo =  http://%OCP_APP%.%HOST_IP%.xip.io/business-central      =
+echo =  http://%OCP_APP%-%OCP_PRJ%.%HOST_IP%.nip.io/business-central      =
 echo =                                                                  =
 echo =  [ u:erics / p:bpmsuite1! ]                                     =
 echo =                                                                  =
